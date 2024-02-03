f7557c4f36a3192dc1bf8a915240381575a1bd81
mr-robot <alenjon2594@gmail.com>
2024-02-03 18:55:46 +0500

    next 2

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from datetime import date
 from fastapi import FastAPI
 from typing import Optional, Union
 
@@ -5,10 +6,15 @@ app = FastAPI()
 
 @app.get('/hotels')
 def get_hotels(
-    location,
-    date_from,
-    date_to,
+    location:str,
+    date_from: str,
+    date_to: str,
     has_spa: bool|None = None,
     stars: int|None = None,
-):
+) -> None:
   return date_from, date_to
+
+
+@app.get("/greet")
+def say_hi(name:  str | None = None):
+    return {"message": f"Hey {name}!"}
