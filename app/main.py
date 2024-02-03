53d2196bc0dba697a34d677f2eceb70a525dbfe2
mr-robot <alenjon2594@gmail.com>
2024-02-03 14:42:49 +0500

    next

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def get_hotels(
     location,
     date_from,
     date_to,
-    has_spa: Union[None, bool] = None,
-    stars: Optional[int] = None,
-) -> None:
+    has_spa: bool|None = None,
+    stars: int|None = None,
+):
   return date_from, date_to
